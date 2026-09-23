36b1f516f2d2d75c38290ed4fc95feca6ba6ed46
Deaddrums <jimhorvath@live.nl>
2026-09-23 12:43:04 +0200

    Pokemon Super class 100% Pokemon classes 100% Main 100%

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,4 +1,20 @@
 package main.java;
 
 public class main {
+
+    public static void  main (String[] args){
+
+    FirePokemon charmander = new FirePokemon("Charmander", 39, 52, "Bessen");
+    WaterPokemon squirtle = new WaterPokemon("Squirtle", 44, 48, "vis");
+    ElectricPokemon pikachu = new ElectricPokemon("Pikachu", 35, 55, "ketchup");
+    GrassPokemon bulbasaur = new GrassPokemon("Bulbasaur", 45, 49, "zonlicht");
+
+    charmander.inferno();
+    squirtle.surf();
+    pikachu.thunder();
+    bulbasaur.solarBeam();
+
+    charmander.eats();
+    squirtle.speaks();
+}
 }
